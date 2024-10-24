7e8f2877593b5f86eb716b7bbf41d6bdb7173402
Melissa Perdomo <melissameira92@gmail.com>
2024-10-24 17:01:37 -0300

    Inserindo produtos no DB.

--- project/database/migrations/20241024152000_insert_producs.php
+++ project/database/migrations/20241024152000_insert_producs.php
@@ -11,7 +11,7 @@ return new class
             (1, 'Ampicilina veterinaria injetavel', 'project/public/imagens/ampicilina-veterinaria-injetavel.png'),
             (2, 'Aminomix gold', 'project/public/imagens/aminomix-gold.png'),
             (3, 'Aurivet', 'project/public/imagens/aurivet.png'),
-            (4, 'Organew pet po', 'project/public/imagens/organew.png');
+            (4, 'Organew pet po', 'imagens/organew.png');
         ");
     }
 
