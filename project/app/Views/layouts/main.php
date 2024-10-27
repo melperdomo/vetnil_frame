568f8f4e77c49676dfec4a3c4ee915f0a1747479
Melissa Perdomo <melissameira92@gmail.com>
2024-10-27 15:40:44 -0300

    Finalizando página de produtos

--- project/app/Views/layouts/main.php
+++ project/app/Views/layouts/main.php
@@ -1,74 +1,72 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Vetnil</title>
-    <link rel="stylesheet" href="/css/layout.css"/>
-  </head>
 
-  <body>
-    <header>
-      
-      <div class="menu-content">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Vetnil</title>
+  <link rel="stylesheet" href="/css/layout.css" />
+</head>
 
-        <div class="menu-left-header">
-          <a class="menu-item" id="">Como Participar</a>
-          <a class="menu-item" id="">Prêmios</a>
-          <a class="menu-item" id="">Produtos Participantes</a>
-        </div>
+<body>
+  <header>
 
-        <div class="menu-central-header">
-          <img
-            class="menu-item-logo"
-            id="logo-home"
-            src="/imagens/imagens-home/2021-logo-vetnil-M.png"
-            alt="logo sorte a mil com vetnil"
-          />
-        </div>
+    <div class="menu-content">
 
-        <div class="menu-right-header">
-          <div class="saudacao">
-            <p class="saudacao-usuario">Olá, Walter!</p>
-            <p class="saudacao-id">ID: 123456</p>
-          
-          </div>
-          <div class="menu-navegacao">
-            <a href=""></a>
-            <a href=""></a>
-            <a href=""></a>
-          </div>
-        </div>
-      
+      <div class="menu-left-header">
+        <a class="menu-item" id="">Como Participar</a>
+        <a class="menu-item" id="">Prêmios</a>
+        <a class="menu-item" id="">Produtos Participantes</a>
       </div>
-    </header>
 
-    <main>
-      
-        <div class="ferraduras">
-          <img
-          class="ferradura-esquerda"
-            name="ferradura-esquerda"
-            id="iferradura-esquerda"
-            src="/imagens/imagens-home/2021-elementos-coloridos-vetnil.png"
-            alt="ferradura-vetnil"
-          />
-          <img
-            class="ferradura-direita"
-            name="ferradura-direita"
-            id="iferradura-direita"
-            src="/imagens/imagens-home/2021-elementos-coloridos-vetnil.png"
-            alt="ferradura-vetnil"
-          />
+      <div class="menu-central-header">
+        <img
+          class="item-logo"
+          id="logo-home"
+          src="/images/images-home/2021-logo-vetnil-M.png"
+          alt="logo sorte a mil com vetnil" />
+      </div>
+
+      <div class="menu-right-header">
+        <div class="saudacao">
+          <p class="saudacao-usuario">Olá, Walter!</p>
+          <p class="saudacao-id">ID: 123456</p>
+
         </div>
+        <div class="menu-navegacao">
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </div>
+      </div>
 
-      <?php include $view_content; ?>
+    </div>
+  </header>
 
-<!-- 
-      <div class="main-como-participar">
+  <main>
+
+    <div class="ferraduras">
+      <img
+        class="ferradura-esquerda"
+        name="ferradura-esquerda"
+        id="iferradura-esquerda"
+        src="/images/images-home/2021-elementos-coloridos-vetnil.png"
+        alt="ferradura-vetnil" />
+      <img
+        class="ferradura-direita"
+        name="ferradura-direita"
+        id="iferradura-direita"
+        src="/images/images-home/2021-elementos-coloridos-vetnil.png"
+        alt="ferradura-vetnil" />
+    </div>
+
+    <?php include $view_content; ?>
+
+
+    <!-- <div class="main-como-participar">
         <div>
           <svg
-          xmlns="file:///C:/Users/Melissa%20Perdomo/Documents/front_end/vetnil/imagens/imagens-home/receipt.svg"
+          xmlns="file:///C:/Users/Melissa%20Perdomo/Documents/front_end/vetnil/images/images-home/receipt.svg"
         ></svg>
         
           <p>Cadastres suas vendas através da nota fiscal.</p>
@@ -77,22 +75,37 @@
         <p> A cada venda efetuada, você ganha uma raspadinha para concorrer a prêmios instantâneos.</p>
 
         <p>Cada raspadinha dá direito a um número da sorte!</p>
+      </div> -->
+
+  </main>
+
+  <footer>
+    
+    <div class="footer-content">
+
+      <div class="footer-left-logo">
+        <img class="item-logo"
+          src="/images/images-home/2021-logo-vetnil-M.png"
+          alt="sorte-a-mil-com-vetnil" />
+      </div>
+      
+      <div class="footer-central">
+        <a class="btn-regras" id="">Como Participar</a>
+        <a class="btn-premios" id="">Prêmios</a>
+        <a class="btn-usuario" id="">Minha Conta</a>
+        <a class="btn-vendas" id="">Minhas Vendas</a>
+      </div>
+      
+      <div class="footer-right">
+        <a class="btn-produtos" id=""> Produtos Participantes </a>
+        <a class="btn-raspadinhas" id="">Raspadinhas</a>
+        <a class="btn-regulamento" id="">Regulamento</a>
+        <a class="btn-ajuda" id="">Fale Conosco</a>
       </div>
 
-    </main>
-    <footer>
-      <img
-        src="/imagens/imagens-home/2021-logo-vetnil-M.png"
-        alt="sorte-a-mil-com-vetnil"
-      />
-      <a class="btn-regras" id="">Como Participar</a>
-      <a class="btn-premios" id="">Prêmios</a>
-      <a class="btn-usuario" id="">Minha Conta</a>
-      <a class="btn-vendas" id="">Minhas Vendas</a>
-      <a class="btn-produtos" id=""> Produtos Participantes </a>
-      <a class="btn-raspadinhas" id="">Raspadinhas</a>
-      <a class="btn-regulamento" id="">Regulamento</a>
-      <a class="btn-ajuda" id="">Fale Conosco</a>
-    </footer>
-  </body>
-</html> -->
+    </div>
+
+  </footer>
+</body>
+
+</html>
